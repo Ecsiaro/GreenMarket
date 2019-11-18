43889f30150479d27b79b6cf276cc560977e3ee4
Grrenix <grrenix@gmail.com>
2019-11-18 00:55:03 +0000

    Now third person, placeholder character added

--- Source/GreenMarket/Character/SlimeCharacter.cpp
+++ Source/GreenMarket/Character/SlimeCharacter.cpp
@@ -2,44 +2,56 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
-#include "GameFramework/InputSettings.h"
-#include "Kismet/GameplayStatics.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
 
+//////////////////////////////////////////////////////////////////////////
+// ATP_ThirdPersonCharacter
 
-ASlimeCharacter::ASlimeCharacter()
-{
+ASlimeCharacter::ASlimeCharacter() {
 	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
- 
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
 	// set our turn rates for input
 	BaseTurnRate = 45.f;
 	BaseLookUpRate = 45.f;
 
-	// Create a CameraComponent	
-	SlimeCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
-	SlimeCameraComponent->SetupAttachment(GetCapsuleComponent());
-	SlimeCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
-	SlimeCameraComponent->bUsePawnControlRotation = true;
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
 
-void ASlimeCharacter::BeginPlay()
-{
-	// Call the base class  
-	Super::BeginPlay();
-	
-}
+//////////////////////////////////////////////////////////////////////////
+// Input
 
-void ASlimeCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
-{
-	// set up game play key bindings
+void ASlimeCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) {
+	// Set up gameplay key bindings
 	check(PlayerInputComponent);
-
-	// Bind jump events
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	
-	// Bind movement events
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASlimeCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ASlimeCharacter::MoveRight);
 
@@ -52,32 +64,37 @@ void ASlimeCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInp
 	PlayerInputComponent->BindAxis("LookUpRate", this, &ASlimeCharacter::LookUpAtRate);
 }
 
-void ASlimeCharacter::MoveForward(float Value)
-{
-	if (Value != 0.0f)
-	{
-		// add movement in that direction
-		AddMovementInput(GetActorForwardVector(), Value);
-	}
-}
-
-void ASlimeCharacter::MoveRight(float Value)
-{
-	if (Value != 0.0f)
-	{
-		// add movement in that direction
-		AddMovementInput(GetActorRightVector(), Value);
-	}
-}
-
-void ASlimeCharacter::TurnAtRate(float Rate)
-{
+void ASlimeCharacter::TurnAtRate(float Rate) {
 	// calculate delta for this frame from the rate information
 	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 }
 
-void ASlimeCharacter::LookUpAtRate(float Rate)
-{
+void ASlimeCharacter::LookUpAtRate(float Rate) {
 	// calculate delta for this frame from the rate information
 	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ASlimeCharacter::MoveForward(float Value) {
+	if ((Controller != NULL) && (Value != 0.0f)) {
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ASlimeCharacter::MoveRight(float Value) {
+	if ((Controller != NULL) && (Value != 0.0f)) {
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, Value);
+	}
 }
--- Source/GreenMarket/Character/SlimeCharacter.h
+++ Source/GreenMarket/Character/SlimeCharacter.h
@@ -1,44 +1,40 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "SlimeCharacter.generated.h"
 
-class UInputComponent;
-
-UCLASS(config=Game)
-class ASlimeCharacter : public ACharacter
-{
+UCLASS(config = Game)
+class ASlimeCharacter : public ACharacter {
 	GENERATED_BODY()
-	/** First person camera */
+
+	/** Camera boom positioning the camera behind the character */
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* SlimeCameraComponent;
-
-
+	class UCameraComponent* FollowCamera;
 public:
 	ASlimeCharacter();
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseLookUpRate;
-	
-	/** Returns FirstPersonCameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetSlimeCameraComponent() const { return SlimeCameraComponent; }
-
 
 protected:
 
-	virtual void BeginPlay() override;
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
 
-	/** Handles moving forward/backward */
-	void MoveForward(float Val);
-
-	/** Handles strafing movement, left and right */
-	void MoveRight(float Val);
+	/** Called for side to side input */
+	void MoveRight(float Value);
 
 	/**
 	 * Called via input to turn at a given rate.
@@ -52,8 +48,15 @@ protected:
 	 */
 	void LookUpAtRate(float Rate);
 
+protected:
 	// APawn interface
-	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	// End of APawn interface
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
 
